3508fe38f5d2b2ff6c876683fe6c4deafa265a85
arzumammadov1 <mi7fqqg3l@code.edu.az>
2024-03-12 00:41:25 +0400

    express limit

--- ofisart.azBack/index.js
+++ ofisart.azBack/index.js
@@ -27,6 +27,14 @@ app.use(
     );
     next();
   });
+  app.use(express.json({
+    limit: '50mb',
+    verify: (req, res, buf) => {
+      if (buf && buf.length) {
+        req.rawBody = buf.toString();
+      }
+    }
+  }));
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
 app.use(bodyParser.json({ limit: '50mb' }));
