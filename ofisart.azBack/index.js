8f51320b3fbac0f57016e1e289a7169e80462e55
arzumammadov1 <mi7fqqg3l@code.edu.az>
2024-04-03 00:40:26 +0400

    bug fix

--- ofisart.azBack/index.js
+++ ofisart.azBack/index.js
@@ -9,26 +9,21 @@ import { ProjectRouter } from './src/routes/projectRoutes.js';
 
 const app = express();
 const port = 3030;
-const frontAddresses = ["https://www.ofisart.az", "https://ofisart.az"]; // İzin verilen ön uç adresleri
+const frontAddress = "http://0.0.0.0";
 
-const corsConfig = {
-  origin: frontAddresses,
-  credentials: true,
-  methods: ["GET", "POST", "PUT", "DELETE"],
-};
-
-app.use(cors(corsConfig));
+app.use(
+    cors({
+        credentials: true,
+        origin: frontAddress,
+    })
+);
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", frontAddress);
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
-app.use((req, res, next) => {
-  if (frontAddresses.includes(req.headers.origin)) {
-    res.setHeader("Access-Control-Allow-Origin", req.headers.origin);
-  }
-  res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
-
 app.use('/api/category', CategoryRouter);
 app.use('/api/product', ProductRouter);
 app.use('/api/admin', AdminRouter);
