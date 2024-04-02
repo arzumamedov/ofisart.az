a43406fa5eacf2bd01e3e62a92a2b36571340606
arzumammadov1 <mi7fqqg3l@code.edu.az>
2024-04-03 00:37:13 +0400

    bug fix

--- ofisart.azBack/index.js
+++ ofisart.azBack/index.js
@@ -2,59 +2,41 @@ import cors from 'cors';
 import 'dotenv/config';
 import express from 'express';
 import mongoose from 'mongoose';
-// import multer from "multer";
-// const multer = require('multer');
-
-// import bodyParser from 'body-parser'
 import { AdminRouter } from './src/routes/adminRoutes.js';
 import { CategoryRouter } from './src/routes/categoryRoutes.js';
 import { ProductRouter } from './src/routes/productRoutes.js';
 import { ProjectRouter } from './src/routes/projectRoutes.js';
-// import { NewProductRouter } from './src/routes/newproductRoutes.js';
-// const upload = multer({ dest: 'public/' })
-const app = express()
-const port = 3030
-const frontAdress = "https://ofisart.az";
+
+const app = express();
+const port = 3030;
+const frontAddresses = ["https://www.ofisart.az", "https://ofisart.az"]; // İzin verilen ön uç adresleri
+
 const corsConfig = {
-  origin: "*",
+  origin: frontAddresses,
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE"],
-}
-app.use(
-  cors(corsConfig)
-);
-// app.options("",cors(corsConfig))
+};
+
+app.use(cors(corsConfig));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", frontAdress);
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
+  if (frontAddresses.includes(req.headers.origin)) {
+    res.setHeader("Access-Control-Allow-Origin", req.headers.origin);
+  }
+  res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
-// app.use(express.json({
-//   limit: '50mb',
-//   verify: (req, res, buf) => {
-//     if (buf && buf.length) {
-//       req.rawBody = buf.toString();
-//     }
-//   }
-// }));
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }));
-// app.use(bodyParser.json({ limit: '50mb' }));
-// app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
-// app.use(express.static('public'))
-app.use('/api/category', CategoryRouter)
-app.use('/api/product', ProductRouter)
-app.use('/api/admin', AdminRouter)
-app.use('/api/project', ProjectRouter)
-// app.use('/api/newproduct',NewProductRouter)
 
+app.use('/api/category', CategoryRouter);
+app.use('/api/product', ProductRouter);
+app.use('/api/admin', AdminRouter);
+app.use('/api/project', ProjectRouter);
 
 mongoose.connect('mongodb+srv://arzu:arzu@cluster0.9p2kmwb.mongodb.net/')
   .then(() => console.log('Connected!'));
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
