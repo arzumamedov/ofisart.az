1696d65197260319805c62da2de8a94bb56c4a7a
arzumammadov1 <mi7fqqg3l@code.edu.az>
2024-05-12 01:51:49 +0400

    bug fixed 2

--- ofisart.azFront/src/pages/Accessory/Accessory.jsx
+++ ofisart.azFront/src/pages/Accessory/Accessory.jsx
@@ -10,7 +10,7 @@ function Accessory() {
     const [detail, setDetail] = useState(null)
 
     useEffect(() => {
-        fetch("https://ofisart-az-api.vercel.app/api/product/65cf41eb2072888a8aa42e6a")
+        fetch("https://ofisart-api.vercel.app/api/product/65cf41eb2072888a8aa42e6a")
             .then((res) => res.json())
             .then((api) => setDetail(api))
     }, [])
